fc65dff081e747f21d211e4eb9690f744c51d9f2
Armando Marques <mail@armandomarques.com>
2020-03-02 15:10:09 +0000

    test2

---
+++ template3/precache-manifest.fcfda5fba1047b59d2dfbe007e5b9e9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75fae8f095e04b242af33e32bc6906d",
+    "url": "/template3/index.html"
+  },
+  {
+    "revision": "bdec49fe9ce8e1e064eb",
+    "url": "/template3/static/js/2.bc630eac.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/template3/static/js/2.bc630eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed59162f428b79a767f",
+    "url": "/template3/static/js/main.ffe560b8.chunk.js"
+  },
+  {
+    "revision": "c6be5459bb217198fd5c",
+    "url": "/template3/static/js/runtime-main.86073a18.js"
+  }
+]);
